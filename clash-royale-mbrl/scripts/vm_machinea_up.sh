b9a89fe58f3dd5b5daa516476ffed784e8e15763
Hasanli <a.hasanli@epartment.nl>
2026-01-22 14:08:01 +0100

    docker: add .dockerignore and allow no-build VM restart

--- clash-royale-mbrl/scripts/vm_machinea_up.sh
+++ clash-royale-mbrl/scripts/vm_machinea_up.sh
@@ -9,6 +9,7 @@ Usage (run on the Azure VM):
     --size size12m \
     --logdir /mnt/azureuser/logs_dreamerv3 \
     --dumpdir /mnt/azureuser/dumps
+    --no-build
 
 Notes:
   - Uses docker-compose file: docker/machineA/docker-compose.vm.yml
@@ -20,6 +21,7 @@ PORT=50051
 SIZE=size12m
 LOGDIR=/mnt/azureuser/logs_dreamerv3
 DUMPDIR=/mnt/azureuser/dumps
+BUILD=1
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -27,6 +29,7 @@ while [[ $# -gt 0 ]]; do
     --size) SIZE="$2"; shift 2;;
     --logdir) LOGDIR="$2"; shift 2;;
     --dumpdir) DUMPDIR="$2"; shift 2;;
+    --no-build) BUILD=0; shift 1;;
     -h|--help) usage; exit 0;;
     *) echo "Unknown arg: $1" >&2; usage; exit 2;;
   esac
@@ -43,7 +46,11 @@ export CR_SIZE="$SIZE"
 sudo mkdir -p "$HOST_LOGDIR" "$HOST_DUMPDIR"
 sudo chown -R "$USER:$USER" "$HOST_LOGDIR" "$HOST_DUMPDIR"
 
-sudo -E docker-compose -f docker/machineA/docker-compose.vm.yml up -d --build
+if [[ "${BUILD}" -eq 1 ]]; then
+  sudo -E docker-compose -f docker/machineA/docker-compose.vm.yml up -d --build
+else
+  sudo -E docker-compose -f docker/machineA/docker-compose.vm.yml up -d
+fi
 
 echo
 echo "Running containers:"
@@ -51,4 +58,3 @@ sudo docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}" | sed -n '1,
 echo
 echo "Tail logs:"
 sudo docker logs --tail 50 -f machinea_machinea_1
-
